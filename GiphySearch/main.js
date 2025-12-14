b288c96f9a09001d206733840c2fa12deefd4c69
Melissa127405 <melissastackpole@gmail.com>
2025-12-14 17:51:58 -0500

    html

---
+++ GiphySearch/main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {Element.getElementById('search-form').addEventListener('submit', handleSearch);}) 
+document.getElementsByClassName('gridcontainer');
+// Fetch gifs from Giphy API and display them
+function handleSearch(event) {confirm('Are you sure you want to search for gifs?'); event.preventDefault(); const searchTerm = document.getElementById('search-input').value; fetchGifs(searchTerm);}
+
+function fetchGifs(searchTerm) {
+    const apiKey = '1F6PjTQW3iN1eMsezymO6DPWQVGouYgU';
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => displayGifs(data.data))
+        .catch(error => console.error('Error:', error));
+}
+                               
+function displayGifs(gifs) {
+    const responseContainer = document.getElementById('response-container');    
+    responseContainer.innerHTML = 'https://api.giphy.com/v1/gifs/search?api_key=1F6PjTQW3iN1eMsezymO6DPWQVGouYgU&q=OMG&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
+    gifs.forEach(gif => {
+        const imageElement = document.createElement('img');
+        imageElement.src = gif.images.fixed_width_small_still.url;
+        responseContainer.appendChild(imageElement);
+    });
+}
+
+
+
+
+
