a690902b37533b200c2d2ef8cccd30d106d19875
Melissa127405 <melissastackpole@gmail.com>
2025-12-16 19:44:11 -0500

    cleanup

--- GiphySearch/main.js
+++ GiphySearch/main.js
@@ -1,6 +1,6 @@
 
 function fetchGifs() {
-    const apiKey = '1F6PjTQW3iN1eMsezymO6DPWQVGouYgU';
+    const apiKey = 'M1WDUb8rs2x3GOzhjQTSx3mbDNCy9liR';
     let searchTerm = document.getElementById('search-OMG').value;
     fetch(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}`)
         .then(response => response.json())
@@ -16,4 +16,12 @@ function displayGifs(gifs) {
         imageElement.src = gif.images.fixed_width_small_still.url;
         responseContainer.appendChild(imageElement);
     });
+}
+
+function clearGifs() {
+  // Clear the input field
+  document.getElementById("search-OMG").value = "";
+
+  // Clear the GIF results
+  document.getElementById("giphyImages").innerHTML = "";
 }
